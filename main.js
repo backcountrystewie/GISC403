0f15cd832d5c074f95d4211c09ade4f21c538ed7
backcountrystewie <162214232+backcountrystewie@users.noreply.github.com>
2024-03-23 12:53:58 +1300

    Update main.js

--- main.js
+++ main.js
@@ -18,21 +18,51 @@ const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/gdmckenzie/clu1ojhk0
 // There are four markers below.  Each has a latitude and longitude.  Edit these to be your unique places of interest.
 // Also note that each has its own 'pop-up.'  Edit the text in each of these to say why each location is important to you.
 
-const blueIcon = L.icon({
+const greenIcon = L.icon({
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [20, 33],
+    iconSize: [20, 33], // Adjust the size here
     iconAnchor: [10, 33],
     popupAnchor: [1, -25],
     shadowSize: [33, 33]
 });
 
 
-const marker1 = L.marker([-43.64051695659158, 172.4788262839913], { icon: blueIcon }).addTo(map)
-	.bindPopup().openPopup();
+const marker1 = L.marker([-46.835782, 168.087044], { icon: greenIcon }).addTo(map)
+	.bindPopup('Port William Hut').openPopup();
 
-const marker5 = L.marker([-43.522405944685644, 172.56241045637384], { icon: blueIcon }).addTo(map)
-	.bindPopup().openPopup();
+const marker2 = L.marker([-46.809016, 168.033516], { icon: greenIcon }).addTo(map)
+	.bindPopup('Bungaree Hut').openPopup();
+
+const marker3 = L.marker([-46.74367710577063, 167.97947321980834], { icon: greenIcon }).addTo(map)
+	.bindPopup('Christmas Village Hut').openPopup();
+
+const marker4 = L.marker([-46.69441228872714, 167.88810637204733], { icon: greenIcon }).addTo(map)
+	.bindPopup('Yankee River Hut').openPopup();
+
+const marker5 = L.marker([-46.693904867453604, 167.80751394332577], { icon: greenIcon }).addTo(map)
+	.bindPopup('Long Harry Hut').openPopup();
+
+const marker6 = L.marker([-46.723535, 167.746077], { icon: greenIcon }).addTo(map)
+	.bindPopup('East Ruggedy Hut').openPopup();
+
+const marker7 = L.marker([-46.74604429383325, 167.7197324986978], { icon: greenIcon }).addTo(map)
+	.bindPopup('Worst Camping Spot Ever').openPopup();
+
+const marker8 = L.marker([-46.814066854976375, 167.738023723526], { icon: greenIcon }).addTo(map)
+	.bindPopup('Big Hellfire Hut').openPopup();
+
+const marker9 = L.marker([-46.926458044406964, 167.7800217374456], { icon: greenIcon }).addTo(map)
+	.bindPopup('Mason Bay Hut').openPopup();
+
+const marker10 = L.marker([-47.032875786621766, 167.70606121628208], { icon: greenIcon }).addTo(map)
+	.bindPopup('Doughboy Bay Hut').openPopup();
+
+const marker11 = L.marker([-46.98201796230116, 167.88082912818572], { icon: greenIcon }).addTo(map)
+	.bindPopup('Rakiahua Hut').openPopup();
+
+const marker12 = L.marker([-46.9282679487012, 167.97746582884804], { icon: greenIcon }).addTo(map)
+	.bindPopup('Freds Camp Hut').openPopup();
 
 
 // These are the Territorial Authority Polygons that are being accessed from a 3rd party server.
